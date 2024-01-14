294694bb16e24d0f6f2bd3358799ca44f8005680
jis3r <jis3r@protonmail.com>
2024-01-14 14:49:13 +0100

    add: tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="text-emerald-600 text-extrabold text-6xl">How many lines of code</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
